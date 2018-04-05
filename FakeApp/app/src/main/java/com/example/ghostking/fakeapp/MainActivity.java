0dedae10b656cc7bc447bba77db1a1e5c1b11507
Santos Martins David <santosmartinsdavid@gmail.com>
2018-04-05 16:58:25 +0200

    Coin A with bottom button added

--- FakeApp/app/src/main/java/com/example/ghostking/fakeapp/MainActivity.java
+++ FakeApp/app/src/main/java/com/example/ghostking/fakeapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.ghostking.fakeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_bottom)
+    Button buttonBottom;
+    @BindView(R.id.coin_a)
+    ImageView coinA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_bottom)
+    public void onViewClicked() {
+
+        coinA.setImageResource(R.drawable.coina);
     }
 }
