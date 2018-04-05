bd3e3be8e84af214c9e3415d857cb74d51dc19ce
NevadaDiamant47 <nevadadiamant@gmail.com>
2018-04-05 16:58:40 +0200

    Coin B button on top

--- FakeApp/app/src/main/java/com/example/ghostking/fakeapp/MainActivity.java
+++ FakeApp/app/src/main/java/com/example/ghostking/fakeapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.ghostking.fakeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.coin_b)
+    ImageView coinB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+    }
+
+    @OnClick(R.id.button)
+    public void onViewClicked() {
+
+        coinB.setImageResource(R.drawable.the_coin);
+        
+
+
+
     }
 }
